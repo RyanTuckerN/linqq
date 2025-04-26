c1d19ca0a60516f7735d9128fac24aa7312b27f3
Ryan Tucker <rtucker@spotinc.com>
2025-04-26 16:38:48 -0400

    remove unused exceptions

--- src/validator/exception.ts
+++ src/validator/exception.ts
@@ -6,14 +6,11 @@ export class Exception extends Error {
   public static SEQUENCE_EMPTY = "Sequence contains no elements" as const;
   public static NO_MATCH = "Sequence contains no matching element" as const;
   public static SEQUENCE_MULTIPLE = "Sequence contains more than one element" as const;
-  public static NOT_IMPLEMENTED = "Method is not implemented" as const;
   public static INDEX_OUT_OF_RANGE = "Index out of range" as const;
   public static ARGUMENT_NULL = (paramName: string) => `Argument ${paramName} cannot be null.` as const;
   public static invalidOperation = (message: string): Exception => new InvalidOperationException(message);
   public static sequenceEmpty = (): Exception => new InvalidOperationException(Exception.SEQUENCE_EMPTY);
-  public static sequenceEmptyOrNoMatch = (): Exception => new InvalidOperationException(Exception.NO_MATCH);
   public static moreThanOne = (): Exception => new InvalidOperationException(Exception.SEQUENCE_MULTIPLE);
-  public static notImplemented = (): Exception => new InvalidOperationException(Exception.NOT_IMPLEMENTED);
   public static indexOutOfRange = (): Exception => new IndexOutOfRangeException(Exception.INDEX_OUT_OF_RANGE);
   public static argument = (message: string): Exception => new ArgumentException(message);
   public static argumentNull = (paramName: string): Exception =>
